1ee7ffec56db9f35d022efa0204abdc9fab4e7df
Kaitlyn Gaynor <gaynor@nceas.ucsb.edu>
2020-01-08 17:04:07 -0800

    Created app file, copied from app.R

--- app.R
+++ app.R
@@ -7,6 +7,7 @@ library(shiny)
 library(shinythemes)
 library(here)
 library(overlap)
+library(plotly)
 
 setwd(here::here("shiny-rai"))
 
@@ -43,6 +44,44 @@ timeplot <-function (A, n.grid = 128, kmax = 3, linecol = "#00BFC4",  ...)
   return(invisible(list(x = xx, densityA = densA)))
 }
 
+# define RAI calculation function
+rai.calculate <- function(record.table, camop) {
+  
+  # calculate how long the camera was functioning in that time period
+  
+  # selects columns within specified dates
+  camop.subset <- dplyr::select(camop, Camera, start.date:end.date) 
+  
+  # sum rows within specified dates (there are 1s when camera was operating, NA when not)
+  camop.subset$Operation <- rowSums(dplyr::select(camop.subset, start.date:end.date), na.rm=TRUE) 
+  
+  # get rid of the individual day columns, just select Camera, Operation
+  camop.subset <- dplyr::select(camop.subset, Camera, Operation) 
+  
+  # calculate number of observations of each classification type at each camera
+  records <- record.table.subset %>%
+    dplyr::group_by(Classification, Camera) %>%
+    dplyr::summarise(Detections = n()) %>%     # counts number of observations of each species
+    spread(key = Classification, value = Detections)  # gets from long to wide format  
+  
+  # add columns for classes not present
+  records <- add_column(records, !!!allclassifications[!names(allclassifications) %in% names(records)])
+  
+  # gather data so each class-camera is its own row again
+  records <- records %>% gather(2:ncol(records), key = "Class", value = "Count")
+  
+  # replace NA with 0 
+  records[is.na(records)] <- 0
+  
+  # join camera operation dates and observations
+  RAI.table <- plyr::join(records, camop.subset)
+  
+  # calculate RAI
+  RAI.table$RAI <- RAI.table$Count / RAI.table$Operation
+  
+  return(RAI.table)
+  
+}
 
 # UI ----------------------------------------------------------------------
 
@@ -100,7 +139,10 @@ ui <- fluidPage(
 # Server ------------------------------------------------------------------
 
 server <- function(input, output) {
-  
+ 
+# Subset based on widgets -------------------------------------------------
+   
+  # create reactive object records_subset that changes based on delta time and date range
   records_subset <- reactive({
     records %>%
       filter(delta.time.secs == 0 | delta.time.secs >= (60 * input$independent_min),
@@ -113,6 +155,15 @@ server <- function(input, output) {
       filter(CommName_Full == input$species_select)
   })
 
+  # create reactive object camera_subset that changes based on camera_select widget selection 
+  camera_subset <- reactive({
+    records_subset() %>%
+      filter(Camera == input$camera_select)
+  })
+
+# Summarize subset data ---------------------------------------------------
+
+  
   # summarize species counts across cameras
   species_summary <- reactive({
     species_subset() %>%
@@ -120,16 +171,8 @@ server <- function(input, output) {
       summarise(count = n())
   })
   
-  # render a reactive table that shows a summary
-  output$species_table <- renderTable({
-    species_summary()
-  })
   
-  # create reactive object camera_subset that changes based on camera_select widget selection 
-  camera_subset <- reactive({
-    records_subset() %>%
-      filter(Camera == input$camera_select)
-  })
+
   
   # summarize species counts across species
   camera_summary <- reactive({
@@ -137,8 +180,16 @@ server <- function(input, output) {
       group_by(CommName_Full) %>%
       summarise(count = n())
   })
+
+
+# Render outputs ----------------------------------------------------------
+
+  # render a reactive table that shows a summary by species
+  output$species_table <- renderTable({
+    species_summary()
+  })
   
-  # render a reactive table that shows a summary
+  # render a reactive table that shows a summary by camera
   output$camera_table <- renderTable({
     camera_summary()
   })
