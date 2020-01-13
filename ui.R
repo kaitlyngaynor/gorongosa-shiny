981a7ec229cd482ce3805d43ae07490e1ed7933c
Kaitlyn Gaynor <gaynor@nceas.ucsb.edu>
2020-01-13 13:57:37 -0800

    Fixed tab issue, added footer

--- ui.R
+++ ui.R
@@ -12,12 +12,34 @@ source("global.R")
 
 sidebar <- dashboardSidebar(
   
+  sidebarMenu(
+    menuItem("By Species", tabName = "species"),
+    menuItem("Species Comparison", tabName = "species_compare")
+  ),
+  
   numericInput(inputId = "independent_min",
                label = "Set quiet period for independent detections (minutes):",
                value = 15,
                min = 0,
-               max = 1440)
+               max = 1440),
   
+  tags$footer(
+    p(
+      "Developed by ",
+      a(href = 'https://www.kaitlyngaynor.com/', "Kaitlyn Gaynor.")
+    ),
+
+    align = "left",
+    style = "
+            position:absolute;
+            bottom:0;
+            width:100%;
+            height:50px; /* Height of the footer */
+            color: white;
+            padding: 10px;
+            background-color: black;
+            z-index: 1000;"
+  )
 )
 
 
@@ -25,27 +47,55 @@ sidebar <- dashboardSidebar(
 
 body <- dashboardBody(
   
-  fluidRow(
-    
-    box(
-      title = "Subset records:",
+  tabItems(
+
+# By species --------------------------------------------------------------
+  
+    tabItem(
+  
+      tabName = "species",
       
-      dateRangeInput(inputId = "date_range1",
-                     label = "Date Range:",
-                     start = "2016-06-01",
-                     end = "2017-09-30"),
-      
-      selectInput(inputId = "species_select",
-                  label = "Choose a species:",
-                  choices = unique(records$CommName_Full))
+      fluidRow(
+        
+        box(
+          title = "Subset records:",
+          
+          dateRangeInput(inputId = "date_range1",
+                         label = "Date Range:",
+                         start = "2016-06-01",
+                         end = "2017-09-30"),
+          
+          selectInput(inputId = "species_select",
+                      label = "Choose a species:",
+                      choices = unique(records$CommName_Full))
+        ),
+        
+        box(
+          
+          title = "Diel activity pattern",
+          
+          plotOutput(outputId = "activity_plot")
+          
+        )
+      )
     ),
-    
-    box(
+  
+  # Species comparison ------------------------------------------------------
+  
+    tabItem(
       
-      title = "Diel activity pattern",
+      tabName = "species_compare",
       
-      plotOutput(outputId = "activity_plot")
-      
+      fluidRow(
+        
+        box(
+          title = "TEST:"
+        ),
+        
+        box(
+          title = "TEST"
+        )
+      )
     )
   )
 )
