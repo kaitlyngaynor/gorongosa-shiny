698970ad54d42b3af9cf9f39d1ee1be51e5aa7c3
Kaitlyn Gaynor <gaynor@nceas.ucsb.edu>
2020-01-17 17:50:03 -0800

    Troubleshooting map labels

--- archive/maptest.R
+++ archive/maptest.R
@@ -55,6 +55,7 @@ library(magrittr)
 library(leaflet)
 library(sf)
 
+# note that this isn't accurate because it includes ALL records and not just the ones within the subset dates; the rai.calculate function works with an already-subset record table
 RAI.test <- records %>% 
   rai.calculate(camera_operation_matrix, "2017-01-01", "2018-01-01") %>%
   filter(Species == "Waterbuck")
@@ -72,6 +73,13 @@ hexes_rai <- full_join(hexes, RAI.test)
 pal <-  colorNumeric(palette = "viridis", domain = hexes_rai$RAI)
 #})
 
+# create map labels
+#map_labels <- reactive({
+map_labels <-  sprintf(
+    "<strong>Camera: %s</strong><br/>Days operating: %i<br/>Detections: %i<br/>RAI: %g",
+    hexes_rai$Camera, hexes_rai$Operation, hexes_rai$Count, hexes_rai$RAI, 0) %>% lapply(htmltools::HTML)
+#})
+
 leaflet(hexes_rai) %>%
   setView(34.42, -18.95, 11) %>%
   addTiles() %>% # or satellite image: addProviderTiles(providers$Esri.WorldImagery)
@@ -80,8 +88,14 @@ leaflet(hexes_rai) %>%
     fillColor = ~pal(hexes_rai$RAI),
     fillOpacity = 1, 
     weight = 1, # stroke weight of lines
-    color = "gray" # color of lines
-  ) %>% 
+    color = "gray", # color of lines
+    label = map_labels,
+    highlight = highlightOptions(
+      weight = 2,
+      color = "white",
+      fillOpacity = 1,
+      bringToFront = TRUE)
+    ) %>% 
   addLegend_decreasing(pal = pal, 
             values = ~RAI,
             opacity = 1, 
